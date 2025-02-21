2013465706bf7e8500b00265f648e6d644cfb5db
Spaces-dev <simplicityspaces@gmail.com>
2025-02-21 15:43:35 +0100

    Initial Commit

---
+++ VoyatekTestUI/VoyatekTestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyatekTestUIApp.swift
+//  VoyatekTestUI
+//
+//  Created by Mac on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct VoyatekTestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoyatekTestUITests/VoyatekTestUITests.swift
@@ -0,0 +1,17 @@
+//
+//  VoyatekTestUITests.swift
+//  VoyatekTestUITests
+//
+//  Created by Mac on 2/21/25.
+//
+
+import Testing
+@testable import VoyatekTestUI
+
+struct VoyatekTestUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
